e45c6664603e2e70c0922ec50b459d4d19cfefb6
Shatrov Egor <e.shatrov@bellerage.com>
2021-11-16 18:12:11 +0300

    epizod IV novaya nadezda(nachinaem otcyda)

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React , {Component} from "react";
+import mapboxgl from "mapbox-gl";
+import "../styles/Map.css"
 
-const Map = () => {
-    return(
-        <div>
-            <h2>go</h2>
-        </div>
-    )
+export class Map extends Component {
+    map = null;
+    mapContainer = React.createRef();
+
+    componentDidMount() {
+        mapboxgl.accessToken = 'pk.eyJ1Ijoic2hlZzEzIiwiYSI6ImNrdzF5bmgxeTFhNGUzMG50aHgxa203M2sifQ.-SW3PFyatqM-YiPIQdduow'
+
+        this.map = new mapboxgl.Map({
+            container: this.mapContainer.current,
+            style: "mapbox://styles/mapbox/streets-v9",
+            center: [30.3056504, 59.9429126],
+            zoom: 10,
+        })
+    }
+
+    componentWillUnmount() {
+        this.map.remove()
+    }
+
+    render() {
+       return <div className="map-wrapper">
+           <div data-testid="map" className="map" ref={this.mapContainer}></div>
+       </div>
+    }
 }
 
-export default Map;
+export default Map; 
