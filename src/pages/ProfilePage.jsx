720ba217cdfa9d705d84e0147242f49f3978d1d7
Shatrov Egor <e.shatrov@bellerage.com>
2021-11-17 14:27:31 +0300

    + map in profile

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "../components/Header"
 import Profile from "../components/Profile"
+import Map from "../components/Map"
+
 
 const ProfilePage = () => {
     return (
-        <div>
-            <Header/>
-            <Profile/>
+        <div className="container-prof">
+            <Map/>
+            <div className="header-prof">
+                <Header/>
+            </div>
+            <div className="profile-prof">
+                <Profile/>
+            </div>     
         </div>
     )
 }
